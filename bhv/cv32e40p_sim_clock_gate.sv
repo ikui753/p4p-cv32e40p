488ff0ff615f1de854b2778bfb78d7f795a74a44
Ian Kuik <ikui753@aucklanduni.ac.nz>
2024-06-27 14:37:12 +1200

    enabled pulp, disabled warnings

--- bhv/cv32e40p_sim_clock_gate.sv
+++ bhv/cv32e40p_sim_clock_gate.sv
@@ -22,7 +22,9 @@ module cv32e40p_clock_gate (
   logic clk_en;
 
   always_latch begin
+    // verilator lint_off COMBDLY
     if (clk_i == 1'b0) clk_en <= en_i | scan_cg_en_i;
+    // verilator lint_on COMBDLY
   end
 
   assign clk_o = clk_i & clk_en;
--- rtl/cv32e40p_core.sv
+++ rtl/cv32e40p_core.sv
@@ -31,9 +31,9 @@
 module cv32e40p_core
   import cv32e40p_apu_core_pkg::*;
 #(
-    parameter PULP_XPULP          =  0,                   // PULP ISA Extension (incl. custom CSRs and hardware loop, excl. p.elw)
-    parameter PULP_CLUSTER = 0,  // PULP Cluster interface (incl. p.elw)
-    parameter FPU = 0,  // Floating Point Unit (interfaced via APU interface)
+    parameter PULP_XPULP          =  1,                   // PULP ISA Extension (incl. custom CSRs and hardware loop, excl. p.elw)
+    parameter PULP_CLUSTER = 1,  // PULP Cluster interface (incl. p.elw)
+    parameter FPU = 1,  // Floating Point Unit (interfaced via APU interface)
     parameter PULP_ZFINX = 0,  // Float-in-General Purpose registers
     parameter NUM_MHPMCOUNTERS = 1
 ) (
